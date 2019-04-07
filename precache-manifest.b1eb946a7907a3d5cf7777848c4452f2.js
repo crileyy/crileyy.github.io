321694e42c1a21c3d2685284517295a09dd4cfa1
crileyy <criley0007@gmail.com>
2019-04-07 14:05:12 -0700

    Updates

---
+++ precache-manifest.b1eb946a7907a3d5cf7777848c4452f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6e43d135e1eacbb68dad776eb69424",
+    "url": "/static/media/headshot.0b6e43d1.JPG"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "89151965f51596ba19bc",
+    "url": "/static/js/main.c5257096.chunk.js"
+  },
+  {
+    "revision": "ffdfee52576f567c2c68",
+    "url": "/static/js/2.261dd553.chunk.js"
+  },
+  {
+    "revision": "89151965f51596ba19bc",
+    "url": "/static/css/main.9b7cdb21.chunk.css"
+  },
+  {
+    "revision": "e577cc047a189be358e29137217ed9f6",
+    "url": "/index.html"
+  }
+];
