d015f3111b3c1663adc55992e5ffa88c6bb613de
crileyy <criley0007@gmail.com>
2020-04-30 11:54:03 -0400

    Updates

---
+++ precache-manifest.2af8d7ad8272587f60098da0831cb930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6e43d135e1eacbb68dad776eb69424",
+    "url": "/static/media/headshot.0b6e43d1.JPG"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "439bdb216991b7b7543a",
+    "url": "/static/js/main.dac12a77.chunk.js"
+  },
+  {
+    "revision": "c6ce52fdf2deb9ff0c4e",
+    "url": "/static/js/2.7df47799.chunk.js"
+  },
+  {
+    "revision": "439bdb216991b7b7543a",
+    "url": "/static/css/main.b49ed239.chunk.css"
+  },
+  {
+    "revision": "48cae90c6cc5c4bcce7b7b4c29569cca",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1eb946a7907a3d5cf7777848c4452f2.js"
+  "/precache-manifest.2af8d7ad8272587f60098da0831cb930.js"
 );
 
 workbox.clientsClaim();
